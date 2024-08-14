64d0ceb8fe8edb5358580bf6d8126e1114191a0e
1trickPwnyta <sarahpace37@gmail.com>
2024-08-13 21:46:09 -0700

    Only override GetParent when both parents are same gender

--- 1.5/Source/Patch_ParentRelationUtility.cs
+++ 1.5/Source/Patch_ParentRelationUtility.cs
@@ -10,40 +10,23 @@ namespace UniversalPregnancy
     [HarmonyPatch("GetParent")]
     public static class Patch_ParentRelationUtility_GetParent
     {
-        public static bool Prefix(Pawn pawn, Gender parentGender, ref Pawn __result)
+        public static void Postfix(Pawn pawn, Gender parentGender, ref Pawn __result)
         {
-            if (!pawn.RaceProps.IsFlesh)
+            if (pawn.relations != null)
             {
-                __result = null;
-                return false;
+                List<Pawn> parents = pawn.relations.DirectRelations.Where(r => r.def == PawnRelationDefOf.Parent).Select(r => r.otherPawn).ToList();
+                if (parents.Count > 1 && parents[0].gender == parents[1].gender)
+                {
+                    if (parentGender == Gender.Male)
+                    {
+                        __result = parents.MinBy(p => p.thingIDNumber);
+                    }
+                    else
+                    {
+                        __result = parents.MaxBy(p => p.thingIDNumber);
+                    }
+                }
             }
-            if (pawn.relations == null)
-            {
-                __result = null;
-                return false;
-            }
-
-            List<Pawn> parents = pawn.relations.DirectRelations.Where(r => r.def == PawnRelationDefOf.Parent).Select(r => r.otherPawn).ToList();
-            if (parents.Count == 0)
-            {
-                __result = null;
-                return false;
-            }
-            if (parents.Count == 1)
-            {
-                __result = parentGender == Gender.Male ? parents[0] : null;
-                return false;
-            }
-
-            if (parentGender == Gender.Male)
-            {
-                __result = parents.MinBy(p => p.thingIDNumber);
-            }
-            else
-            {
-                __result = parents.MaxBy(p => p.thingIDNumber);
-            }
-            return false;
         }
     }
 }
